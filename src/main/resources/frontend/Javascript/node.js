a17a07b1917888711112dee185289c1918c9c419
OwaisFe <210121942@aston.ac.uk>
2023-02-26 19:21:50 +0000

    Filter updates

--- src/main/resources/frontend/Javascript/node.js
+++ src/main/resources/frontend/Javascript/node.js
@@ -1,42 +1,59 @@
 function filterProducts() {
-    const priceRangeFilter = document.getElementById("price-range-filter").value;
-    const sortFilter = document.getElementById("sort-filter").value;
-    const products = document.querySelectorAll(".product-name");
-  
-    for (let i = 0; i < products.length; i++) {
-      const productPrice = parseFloat(products[i].querySelector(".product-price span").textContent.slice(1));
-  
-      // filter by price range
-      if (priceRangeFilter) {
-        const [minPrice, maxPrice] = priceRangeFilter.split("-");
-        if (productPrice < parseFloat(minPrice) || productPrice > parseFloat(maxPrice)) {
-          products[i].style.display = "none";
-          continue;
-        }
-      }
-  
-      products[i].style.display = "block";
+  const priceRangeFilter = document.getElementById("price-range-filter").value;
+  const sortFilter = document.getElementById("sort-filter").value;
+
+  const products = document.querySelectorAll(".product-name");
+
+  // Reset to original products if "relevance" is selected
+  if (sortFilter === "relevance") {
+    const sortedProducts = originalProducts.sort((a, b) => {
+      const aName = a.querySelector("h3").textContent.toLowerCase();
+      const bName = b.querySelector("h3").textContent.toLowerCase();
+      return aName.localeCompare(bName);
+    });
+
+    for (let i = 0; i < sortedProducts.length; i++) {
+      sortedProducts[i].parentNode.appendChild(sortedProducts[i]);
     }
-  
-    // sort products
-    if (sortFilter === "price-low-high") {
-      const sortedProducts = Array.from(products).sort((a, b) => {
-        const priceA = parseFloat(a.querySelector(".product-price span").textContent.slice(1));
-        const priceB = parseFloat(b.querySelector(".product-price span").textContent.slice(1));
-        return priceA - priceB;
-      });
-      for (let i = 0; i < sortedProducts.length; i++) {
-        sortedProducts[i].parentNode.appendChild(sortedProducts[i]);
+
+    return;
+  }
+
+  for (let i = 0; i < products.length; i++) {
+    const productPrice = parseFloat(products[i].querySelector(".product-price span").textContent.slice(1));
+
+    // filter by price range
+    if (priceRangeFilter) {
+      const [minPrice, maxPrice] = priceRangeFilter.split("-");
+      if (productPrice < parseFloat(minPrice) || productPrice > parseFloat(maxPrice)) {
+        products[i].style.display = "none";
+        continue;
       }
-    } else if (sortFilter === "price-high-low") {
-      const sortedProducts = Array.from(products).sort((a, b) => {
-        const priceA = parseFloat(a.querySelector(".product-price span").textContent.slice(1));
-        const priceB = parseFloat(b.querySelector(".product-price span").textContent.slice(1));
-        return priceB - priceA;
-      });
-      for (let i = 0; i < sortedProducts.length; i++) {
-        sortedProducts[i].parentNode.appendChild(sortedProducts[i]);
-      }
+    }
+
+    products[i].style.display = "block";
+  }
+
+  // sort products
+  if (sortFilter === "price-low-high") {
+    const sortedProducts = Array.from(products).sort((a, b) => {
+      const priceA = parseFloat(a.querySelector(".product-price span").textContent.slice(1));
+      const priceB = parseFloat(b.querySelector(".product-price span").textContent.slice(1));
+      return priceA - priceB;
+    });
+
+    for (let i = 0; i < sortedProducts.length; i++) {
+      sortedProducts[i].parentNode.appendChild(sortedProducts[i]);
+    }
+  } else if (sortFilter === "price-high-low") {
+    const sortedProducts = Array.from(products).sort((a, b) => {
+      const priceA = parseFloat(a.querySelector(".product-price span").textContent.slice(1));
+      const priceB = parseFloat(b.querySelector(".product-price span").textContent.slice(1));
+      return priceB - priceA;
+    });
+
+    for (let i = 0; i < sortedProducts.length; i++) {
+      sortedProducts[i].parentNode.appendChild(sortedProducts[i]);
     }
   }
-  
+}
